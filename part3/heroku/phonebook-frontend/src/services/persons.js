af7cefb50bfb0ef439a08d47d48745d7450ffa70
unknown <rodrigo.pueblas@hotmail.com>
2021-02-13 14:10:54 +0100

    Required files for npm

--- part3/heroku/phonebook-frontend/src/services/persons.js
+++ part3/heroku/phonebook-frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'http://localhost:3001/persons'
+const baseUrl = 'https://fullstackopen-part3-rguitar96.herokuapp.com/'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
