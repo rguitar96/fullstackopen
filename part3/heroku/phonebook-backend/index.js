a5d9bf60417d939ebb2538c53ab73343c9c4b1d9
unknown <rodrigo.pueblas@hotmail.com>
2021-02-13 14:13:40 +0100

    Add cors to heroku backend

--- part3/heroku/phonebook-backend/index.js
+++ part3/heroku/phonebook-backend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
+const cors = require('cors')
 
+const app = express()
+app.use(cors())
 app.use(express.json())
 app.use(morgan((tokens, req, res) => 
     [
