9f7a1fc13e365afca7c1ad5ebdc72bac208c504c
unknown <rodrigo.pueblas@hotmail.com>
2021-02-13 15:16:20 +0100

    Proxy added

--- part3/heroku/phonebook-frontend/src/services/persons.js
+++ part3/heroku/phonebook-frontend/src/services/persons.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const baseUrl = 'https://fullstackopen-part3-rguitar96.herokuapp.com/api/persons'
+const baseUrl = '/api/persons'
 
 const getAll = () => {
     const request = axios.get(baseUrl)
